d650298177ca45a6da153dba88eb936a02db05d6
Shama-David Grier-Johnson <127712361+TheSecurityLead@users.noreply.github.com>
2023-12-13 18:25:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
+let rawdata = fs.readFileSync('weather.json');
+let weatherData = JSON.parse(rawdata);
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
